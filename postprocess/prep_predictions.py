7afe3301ef4c86b67515313931207891c5615b16
Vatsal Raina <vatsalraina@hotmail.co.uk>
2021-10-26 14:25:39 +0100

    improved postprocessing

--- postprocess/prep_predictions.py
+++ postprocess/prep_predictions.py
@@ -85,6 +85,12 @@ def main(args):
         input_encodings_dict = tokenizer(concatenation, truncation=True, max_length=512, padding="max_length")
         input_ids = input_encodings_dict['input_ids']
 
+        # Temp
+        combo = question + "[SEP]" + passage
+        input_ids_alt = tokenizer.encode(combo)
+
+        assert input_ids == input_ids_alt
+
         # From first occurence of the SEP token to the last occurence of the SEP token
         context_start_logits = start_logits[input_ids.index(102) + 1  :  -1 * (input_ids[::-1].index(102) + 1) ]
         context_end_logits = end_logits[input_ids.index(102) + 1  :  -1 * (input_ids[::-1].index(102) + 1) ]
