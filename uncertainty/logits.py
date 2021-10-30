1deb9fdecfbb721ef318255e82c00b091b5615a5
yassirf <yf286@cam.ac.uk>
2021-10-30 21:38:43 +0100

    Update logits.py

--- uncertainty/logits.py
+++ uncertainty/logits.py
@@ -186,8 +186,8 @@ class DirichletEnsembleLogits(EnsembleLogits):
         alphas = np.exp(log_alphas)
         alpha0 = alphas.sum(axis = -1)
 
-        entropy = sp.special.gamma(alpha0)/sp.special.gamma(alpha0 + 0.50)
-        entropy *= (sp.special.gamma(alphas + 0.50)/sp.special.gamma(alphas)).sum(axis = -1)
+        entropy = np.exp(sp.special.gammaln(alpha0) - sp.special.gammaln(alpha0 + 0.50))
+        entropy *= (np.exp(sp.special.gammaln(alphas + 0.50) - sp.special.gammaln(alphas))).sum(axis = -1)
 
         return entropy.mean(0)
 
